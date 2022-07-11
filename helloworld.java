9783f969a37fa14add2584659edff3f8c4f41c34
noorshaik9550 <109077179+noorshaik9550@users.noreply.github.com>
2022-07-11 19:05:13 +0530

    if condition added

--- helloworld.java
+++ helloworld.java
@@ -5,5 +5,10 @@ class Hello
     System.out.println("hello world");
     System.out.println("hi");
     System.out.println("bye");
+    
+    if(20<10)
+      System.out.println("first");
+    else
+      System.out.println("second");
   }
 }
